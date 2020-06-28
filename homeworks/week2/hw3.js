102326d70b57c2b4879025fdcdae77d98a961496
Hotaru <hotaru@Hotarude-MacBook-Pro.local>
2020-06-28 22:20:03 +0800

    week2ok

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,19 @@
 function reverse(str) {
-  
+    var answer = ""
+    for (var i = str.length - 1; i >= 0; i--) {
+        answer += str[i]
+    }
+    console.log(answer)   
 }
 
 reverse('hello');
+
+//test
+//reverse('yoyoyo')
+//正確輸出：oyoyoy
+
+//reverse('1abc2')
+//正確輸出：2cba1
+
+//reverse('1,2,3,2,1')
+//正確輸出：1,2,3,2,1
